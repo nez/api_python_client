252d23aea2167bd0461022e7d8bbb05580b25b07
Andres Gomez Urquiza <andres.gomez@datos.mx>
2018-04-10 13:49:25 -0500

    setup.py no soporta ñ

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['datosgobmx'], # this must be the same as the name above
   version = '0.1',
   description = 'Connect to api.datos.gob.mx',
-  author = 'Leo Castañeda',
+  author = 'Leo Castaneda',
   author_email = 'leonel.castaneda@datos.mx',
   url = 'https://github.com/mxabierto/api_python_client',
   download_url = 'https://github.com/mxabierto/api_python_client/archive/0.1.tar.gz',
